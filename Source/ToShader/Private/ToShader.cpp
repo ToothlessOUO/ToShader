0c41b6f9fd8ed13df731f09af91f6fc478b7e7df
ToothlessOUO <116816456+ToothlessOUO@users.noreply.github.com>
2025-02-21 18:37:29 +0800

    删除编译文件，开始编写toshader

--- Source/ToShader/Private/ToShader.cpp
+++ Source/ToShader/Private/ToShader.cpp
@@ -2,19 +2,58 @@
 
 #include "ToShader.h"
 
+DEFINE_LOG_CATEGORY(LogToShader);
+
 #define LOCTEXT_NAMESPACE "FToShaderModule"
 
 void FToShaderModule::StartupModule()
 {
-	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+	TickDelegate = FTickerDelegate::CreateRaw(this, &FToShaderModule::Tick);
+	TickDelegateHandle = FTSTicker::GetCoreTicker().AddTicker(TickDelegate);
 }
 
 void FToShaderModule::ShutdownModule()
 {
-	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
-	// we call this function before unloading the module.
+}
+
+void FToShaderModule::tolog(FString msg)
+{
+	UE_LOG(LogToShader, Warning, TEXT("%s"), *msg);
+}
+
+void FToShaderModule::ApplyEngineConfigs()
+{
+	if (GEngine != nullptr && !bHasApplyEnginConfigsDone)
+	{
+		// 检查 r.Nanite.AllowTessellation 是否已经设置为 1
+		bool bAllowTessellationSet = false;
+		GConfig->GetBool(TEXT("/Script/Engine.RendererSettings"), TEXT("r.Nanite.AllowTessellation"), bAllowTessellationSet, GGameIni);
+		tolog("GGameTessellation enabled "+bAllowTessellationSet?"True":"False");
+		if (!bAllowTessellationSet)
+		{
+			GEngine->Exec(nullptr, TEXT("r.Nanite.AllowTessellation = 1"));
+			GConfig->SetBool(TEXT("/Script/Engine.RendererSettings"), TEXT("r.Nanite.AllowTessellation"), true, GGameIni);
+		}
+
+		// 检查 r.Nanite.Tessellation 是否已经设置为 1
+		bool bTessellationSet = false;
+		GConfig->GetBool(TEXT("/Script/Engine.RendererSettings"), TEXT("r.Nanite.Tessellation"), bTessellationSet, GGameIni);
+		if (!bTessellationSet)
+		{
+			GEngine->Exec(nullptr, TEXT("r.Nanite.Tessellation = 1"));
+			GConfig->SetBool(TEXT("/Script/Engine.RendererSettings"), TEXT("r.Nanite.Tessellation"), true, GGameIni);
+		}
+		bHasApplyEnginConfigsDone = true;
+	}
+	
+}
+
+bool FToShaderModule::Tick(float DeltaTime)
+{
+	ApplyEngineConfigs();
+	return false;
 }
 
 #undef LOCTEXT_NAMESPACE
-	
+
 IMPLEMENT_MODULE(FToShaderModule, ToShader)
--- Source/ToShader/Public/ToShader.h
+++ Source/ToShader/Public/ToShader.h
@@ -4,6 +4,8 @@
 
 #include "Modules/ModuleManager.h"
 
+DECLARE_LOG_CATEGORY_EXTERN(LogToShader, Log, All);
+
 class FToShaderModule : public IModuleInterface
 {
 public:
@@ -11,4 +13,15 @@ public:
 	/** IModuleInterface implementation */
 	virtual void StartupModule() override;
 	virtual void ShutdownModule() override;
+
+	private:
+
+	void tolog(FString msg);
+
+	bool bHasApplyEnginConfigsDone = false;
+	void ApplyEngineConfigs();
+
+	FTickerDelegate TickDelegate;
+	FTSTicker::FDelegateHandle TickDelegateHandle;
+	bool Tick(float DeltaTime);
 };
